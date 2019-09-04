584f7e790a46d974dfbf2bc609d86c411bb8a71e
Ambition <wx_93d9bd06b9ad453bb834756db9cb5b40@git.code.tencent.com>
2019-09-04 21:54:10 +0800

    部分包结构

--- src/main/java/com/b303/WaterbarApplication.java
+++ src/main/java/com/b303/WaterbarApplication.java
@@ -9,6 +9,7 @@ public class WaterbarApplication {
     public static void main(String[] args) {
         SpringApplication.run(WaterbarApplication.class, args);
         System.out.println("还不行？");
+        System.out.println("行了吧！");
     }
 
 }
