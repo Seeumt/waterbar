de5748aae6852f4a547c74160997af2f244d296f
REN XIANG <Seeumt@cumt.edu.cn>
2019-09-04 21:15:38 +0800

    Initial commit

---
+++ src/main/java/com/b303/WaterbarApplication.java
@@ -0,0 +1,13 @@
+package com.b303;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaterbarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WaterbarApplication.class, args);
+    }
+
+}
