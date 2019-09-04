eed29eb03c8cc5dd2d2b1c3fc985ca7c45f5e112
REN XIANG <Seeumt@cumt.edu.cn>
2019-09-04 21:52:07 +0800

    ？？

--- src/main/java/com/b303/WaterbarApplication.java
+++ src/main/java/com/b303/WaterbarApplication.java
@@ -8,6 +8,7 @@ public class WaterbarApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WaterbarApplication.class, args);
+        System.out.println("还不行？");
     }
 
 }
